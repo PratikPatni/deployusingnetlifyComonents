1f69652626af8f156309f9bc2425e6275e9846ef
PratikPatni <pratikpatni100@gmail.com>
2020-08-15 22:58:37 +0530

    updated code

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,4 @@
 import React,{useState , useEffect , useRef} from 'react';
-import { findAllByLabelText } from '@testing-library/react';
 
 const Dropdown = ({label,options,selected ,onSelectedChange}) => {
 
